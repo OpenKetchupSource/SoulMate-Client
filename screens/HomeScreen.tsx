000625f4038bb0397570ea0c801425511051405c
jungsunbeen <tjsqls041103@gmail.com>
2025-05-13 17:24:50 +0900

    install NEWgit add . project

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../App';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+export default function HomeScreen({ navigation }: Props) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>🏠 Home 화면입니다!</Text>
+      <Button title="Detail로 이동" onPress={() => navigation.navigate('Detail')} />
+    </View>
+  );
+}
